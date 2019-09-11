e8c1dcadcc32c7b151d8fd2bedd6a8f61174d9b8
Ubuntu <ubuntu@ip-172-31-16-144.ec2.internal>
2019-09-11 18:43:30 +0000

    Updated css created contact and home app

---
+++ contact/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# A Simple Contact Form
+
+class contact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=30)
+    email = models.EmailField()
+    
+    
--- graphic_design_services/settings.py
+++ graphic_design_services/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import env
+import dj_database_url
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -88,6 +92,7 @@ DATABASES = {
     }
 }
 
+#DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
