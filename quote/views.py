fa99f2eba922293baac81867a9482582ac843896
Ubuntu <ubuntu@ip-172-31-16-144.ec2.internal>
2019-09-12 13:36:15 +0000

    update for review2

--- quote/views.py
+++ quote/views.py
@@ -7,11 +7,14 @@ from .forms import GetQuote
 def get_quote(request):
     
     if request.method == "POST":
-        form = GetQuote(request.POST, request.FILES, instance=get_quote)
+        form = GetQuote(request.get_quote, request.FILES, instance=get_quote)
         if form.is_valid():
             get_quote = form.save()
             messages.success(request, "Thank you for your Quotation Request which has been sent sucessfully. We will be in touch soon.")
             return redirect(reverse('index'))
+        
+        else: 
+            return render(request, 'quote.html')
  
         
     
